d92a95e79dff6d9080a8deeb7397821a61c1d376
ParthPatelCa <147653862+ParthPatelCa@users.noreply.github.com>
2025-08-06 16:56:59 -0400

    Add basic pages setup

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to LocalMailer!</h1>
+      <p>This is your basic setup for the project.</p>
+    </div>
+  );
+}
